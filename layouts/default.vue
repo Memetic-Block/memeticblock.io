56d145fcad7733968ea6e69dcf4fd9dcf4a727a8
jsb7131 <jasonstephenbaker@gmail.com>
2023-08-14 00:09:50 -0400

    Vuetify, pages, default and index, beginning of theme colors

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-app class="app">
+        <v-main class="app-main">
+        <slot />
+        </v-main>
+    </v-app>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  ssr: false,
+
+  app: {
+    baseURL: '/',
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'Description',
+          content: 'Memetic Block website'
+        }
+      ]
+    }
+  },
+
+  experimental: {
+    writeEarlyHints: false
+  },
+
+  /**
+   * Vuetify Config
+   * See https://codybontecou.com/how-to-use-vuetify-with-nuxt-3.html
+   * See https://pictogrammers.github.io/@mdi/font/7.1.96/
+   */
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    },
+  },
+
+  /**
+   * Vue Router Config
+   */
+  pages: true
 })
