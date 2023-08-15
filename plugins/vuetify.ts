e80411dbdddc20ef4f3e9799ae7eaaf91bd4d1e3
jsb7131 <jasonstephenbaker@gmail.com>
2023-08-14 22:19:24 -0400

    theme colors

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,43 +2,31 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-let theme = 'dark'
-const localStorageTheme = localStorage && localStorage.getItem('theme')
-if (localStorageTheme) {
-  theme = localStorageTheme
-}
-
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
     components,
     directives,
     theme: {
-      defaultTheme: theme,
+      defaultTheme: 'dark',
       themes: {
         dark: {
             colors: {
               primary: '#C82323',
               secondary: '#df2626',
+              'gray-text': '#999999',
+              surface: '#333333',
               background: '#222222',
+              'chalk-background': '#E1E1E1',
+              'off-white': '#F8F8F8',
+              error: '#FF5252',
+              info: '#2196F3',
+              success: '#4CAF50',
+              warning: '#FFC107',
             }
-        },
-        light: {
-          colors: {
-            primary: '#C82323',
-            secondary: '#df2626',
-            accent: '#000000',
-            background: '#FFFFFF',
-            surface: '#FFFFFF',
-            error: '#FF5252',
-            info: '#2196F3',
-            success: '#4CAF50',
-            warning: '#FFC107',
-          }
         }
       }
     }
   })
-
   nuxtApp.vueApp.use(vuetify)
 })
