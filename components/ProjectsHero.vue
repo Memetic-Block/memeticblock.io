6138bd668a4b444860c73a1aec1ba534829ddf94
jsb7131 <jasonstephenbaker@gmail.com>
2023-08-17 20:33:04 -0400

    down arrow timeout disappear, art by city logo projects hero

--- components/ProjectsHero.vue
+++ components/ProjectsHero.vue
@@ -1,3 +1,7 @@
 <template>
-    <div class="projects-hero surface-bg"></div>
+    <div class="projects-hero surface-bg">
+        <div class="client-logo">
+            <img src="https://artby.city/logo/logo_by_daliah_ammar_dark_transparent.png" alt="Art x By x City" />
+        </div>
+    </div>
 </template>
