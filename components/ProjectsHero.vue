72b78ee0b35df6a2c9365b3e7a3d793efe14f27a
Jim Toth <jim@artby.city>
2023-08-18 20:57:23 -0400

    Refactor downarrow on homepage

--- components/ProjectsHero.vue
+++ components/ProjectsHero.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="projects-hero surface-bg">
+    <div id="projects-hero" class="projects-hero surface-bg">
         <div class="client-logo">
-            <img src="https://artby.city/logo/logo_by_daliah_ammar_dark_transparent.png" alt="Art x By x City" />
+            <img
+                src="/logo/logo_by_daliah_ammar_dark_transparent.png"
+                alt="Art x By x City"
+            />
         </div>
     </div>
 </template>
+
+<style scoped>
+.projects-hero {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 200px;
+    padding: 100px;
+}
+
+.client-logo img {
+    width: 200px;
+    height: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,4 @@
 <template>
     <LandingHero />
     <ProjectsHero />
-    <div class="down-arrow" v-show="showDownArrow">
-        <DownArrow />
-    </div>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-
-const showDownArrow = ref(true)
-let scrollTimeout: number
-
-const handleScroll = () => {
-    if (window.scrollY === 0) {
-        clearTimeout(scrollTimeout)
-        showDownArrow.value = true
-    } else if (showDownArrow.value === true) {
-        clearTimeout(scrollTimeout)
-        scrollTimeout = setTimeout(() => {
-            showDownArrow.value = false
-        }, 1000)
-    }
-}
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll)
-})
-
-onBeforeUnmount(() => {
-  window.removeEventListener('scroll', handleScroll)
-})
-</script>
