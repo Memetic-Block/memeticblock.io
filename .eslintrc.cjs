fe22ba961977e3ad50e0a8a2ed275f9631d3b471
Jim Toth <jim@artby.city>
2023-08-23 19:33:33 -0400

    Enable typescript eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,21 +16,24 @@ module.exports = {
   parserOptions: {
     parser: "@typescript-eslint/parser",
     sourceType: 'module',
-    // project: true,
-    // tsconfigRootDir: __dirname,
+    project: true,
+    tsconfigRootDir: __dirname,
+    extraFileExtensions: [ '.vue' ],
     vueFeatures: {
       interpolationAsNonHTML: false
     }
   },
   plugins: [ '@typescript-eslint' ],
   extends: [
-    // 'eslint:recommended',
-    // 'plugin:vue/base',
     'plugin:vue/vue3-recommended',
-    // 'plugin:@typescript-eslint/recommended'
-    // 'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/recommended-type-checked',
   ],
   rules: {
+    indent: [ 'error', 2 ],
+    'max-len': [ 'error', {
+      code: 80,
+      tabWidth: 2
+    }],
     'vue/multi-word-component-names': 'off',
     'vue/component-tags-order': 'off',
     'vue/max-attributes-per-line': ['warn', {
