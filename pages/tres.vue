ec3d809f0323dca6ccd17bb335a76ce1d8dade40
Jim Toth <jim@artby.city>
2025-02-14 23:15:06 -0500

    wip: tres

--- pages/tres.vue
+++ pages/tres.vue
@@ -1,77 +1,66 @@
 <script setup lang="ts">
-import { TresCanvas, useTexture, useRenderLoop } from '@tresjs/core'
+import { TresCanvas, useTexture, useRenderLoop, useLoader } from '@tresjs/core'
 import { Html, OrbitControls } from '@tresjs/cientos'
-import { Mesh } from 'three'
+import { Mesh, TextureLoader, Texture, SRGBColorSpace } from 'three'
+import { SVGLoader, type SVGResult } from 'three/addons/loaders/SVGLoader.js'
 
-const texture = await useTexture({
-  map: '/logo/memetic-block/memetic-block.png'
-})
+
+const texture: Texture = (await useLoader(TextureLoader as any, '/logo/memetic-block/memetic-block-black.png')) as Texture
+texture.colorSpace = SRGBColorSpace
 
 const { onLoop } = useRenderLoop()
 const cube = ref<Mesh>()
 onLoop(({ delta, elapsed }) => {
   if (cube.value) {
     // cube.value.rotation.y += delta
-    // cube.value.rotation.z = elapsed * 0.1
-    // cube.value.rotation.x += delta
-    cube.value.rotation.y += delta
-    // cube.value.rotation.z += delta
-    // cube.value.rotation.
   }
 })
+const cam1 = [0, 0, 0] as any
+const cam2 = [Math.PI/4, 0, 0] as any
+let currentCam = ref('cam1')
+let cameraRotation = computed(() => { if (currentCam.value === 'cam1') { return cam1 } else { return cam2 } })
+
+function toggleCamera() {
+  console.log('toggleCamera')
+  if (currentCam.value === 'cam2') { currentCam.value = 'cam1' }
+  else { currentCam.value = 'cam2' }
+}
 </script>
 
 <template>
-  <TresCanvas
-    window-size
-    clear-color="#222222"
+  <div
+    style="width: 100%; height: 100%"
+    @click="toggleCamera"
   >
-    <TresPerspectiveCamera :position="[0, 0, 5]" />
-    <TresDirectionalLight :position="[0, 5, 10]" color="#ffffff" />
-    <!-- <OrbitControls /> -->
-
-    <TresMesh :position="[0, 0, -1]">
-      <TresPlaneGeometry :args="[2.5, 2.5]" />
-      <TresMeshBasicMaterial :map="texture.map" />
-      <!-- <Html
-        center
-        transform
-        :distance-factor="1"
-        :position="[0, 0, 0.1]"
-        :scale="[0.75, 0.75, 0.75]"
-      >
-        <iframe frameborder="0" src="/logo/memetic-block/memetic-block.png"></iframe>
-      </Html> -->
-    </TresMesh>
-
-    <TresMesh
-      ref="cube"
-      :position="[0, 0, 0]"
-      :rotation="[Math.PI/3, Math.PI / 4, 0]"
-      cast-shadow
+    <TresCanvas
+      window-size
+      clear-color="#000000"
     >
-      <TresBoxGeometry :args="[3, 3, 3]"/>
-      <TresMeshPhysicalMaterial
-        color="#ffffff"
-        transmission="1"
-        roughness="0.1"
-        thickness="1"
-      />
-    </TresMesh>    
-  </TresCanvas>
-</template>
+      <TresPerspectiveCamera :position="[0, 0, 5]" :rotation="cameraRotation" />
+      <!-- <TresDirectionalLight :position="[0, 5, 10]" color="#ffffff" /> -->
+      <OrbitControls />
 
-<style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-}
-#app {
-  height: 100%;
-  width: 100%;
-  background-color: #000;
-}
-</style>
+      <TresMesh :position="[0, 0, -1]">
+        <TresPlaneGeometry :args="[2.5, 2.5]" />
+        <!-- <TresMeshBasicMaterial :map="texture.map" /> -->
+        <TresMeshBasicMaterial :map="texture"  />
+
+      </TresMesh>
+
+      <TresMesh
+        ref="cube"
+        :position="[0, 0, 0]"
+        :rotation="[Math.PI/2.5, Math.PI / 4, 0]"
+        cast-shadow
+      >
+        <TresBoxGeometry :args="[3, 3, 3]" />
+        <TresMeshPhysicalMaterial
+          color="#ffffff"
+          :transmission="1"
+          :roughness="0"
+          :thickness="1"
+        />
+      </TresMesh>
+    </TresCanvas>
+  </div>
+</template>
