39dd001700cd3556e63c1817910624b35c335a06
Jim Toth <jim@artby.city>
2025-07-16 11:59:23 -0400

    Updates contact form placeholder

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,21 +2,32 @@
   <main>
     <section> 
       <h1 class="text-center pt-12">Memetic Block - Contact Us</h1>
-      <iframe
-        data-w-type="embedded"
-        frameborder="0"
-        scrolling="no"
-        marginheight="0"
-        marginwidth="0"
-        src="https://syxro.mjt.lu/wgt/syxro/0sq8/form?c=6a5f0e82"
-        width="100%"
-        style="height: 0;"
-      >Fetching Contact Form...</iframe>
+      <ClientOnly>
+        <iframe
+          data-w-type="embedded"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src="https://syxro.mjt.lu/wgt/syxro/0sq8/form?c=6a5f0e82"
+          width="100%"
+          :style="{ height: showLoadingMessage ? '0' : '556px' }"
+          @load="onContactFormLoaded"
+        ></iframe>
+        <h3 v-show="showLoadingMessage" class="text-center">
+          Fetching Contact Form...
+        </h3>
+      </ClientOnly>
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
+const showLoadingMessage = ref<boolean>(true)
+const onContactFormLoaded = () => {
+  showLoadingMessage.value = false
+}
+
 useHead({
   meta: [
     { property: "og:title", content: "Contact Us | Memetic Block" },
